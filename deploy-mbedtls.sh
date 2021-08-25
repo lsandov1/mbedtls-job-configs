c3bde6e2c91848b7efda8f3804ef7b32ad304f26
Leonardo Sandoval <leonardo.sandoval@linaro.org>
2021-08-25 14:48:39 -0500

    first commit
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval@linaro.org>

---
+++ deploy-mbedtls.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -xe
+
+yamls=( \
+	lsandov1-mbed-tls-pr-head.yaml \
+        lsandov1-mbed-tls-nightly-tests.yaml \
+        lsandov1-mbed-tls-pr-ci-testing.yaml \
+	lsandov1-mbedtls-pr-multibranch.yaml \
+    )
+
+# migrate jobs
+for yaml in ${yamls[*]}; do
+    # test
+    jenkins-jobs test $yaml -o mbed-tls-output
+
+    # deploy
+    jenkins-jobs --conf=$PWD/lsandov1-jenkins-jobs.ini update $yaml $(basename -s .yaml $yaml)
+done
