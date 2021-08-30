26b0dda11a7a4eb5020dd6eae04797c94501678d
Leonardo Sandoval <leonardo.sandoval@linaro.org>
2021-08-30 11:15:02 -0500

    introduce lsandov1-mbedtls-release-ci-testing.yaml
    TODO fix git credential injection
    
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval@linaro.org>

--- deploy-mbedtls.sh
+++ deploy-mbedtls.sh
@@ -9,6 +9,7 @@ yamls=( \
 	lsandov1-mbedtls-pr-multibranch.yaml \
 	lsandov1-mbed-tls-pr-test-parametrized.yaml \
 	lsandov1-mbedtls-release-ci-testing.yaml \
+	lsandov1-mbedtls-release-new.yaml \
     )
 
 # migrate jobs
